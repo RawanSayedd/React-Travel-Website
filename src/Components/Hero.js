32fc07eb61699610dd58439a765107d6b1b52469
RawanSayedd <rawansayed143@gmail.com>
2023-09-14 18:25:53 +0300

    modif.

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -6,7 +6,6 @@ import { Button } from "./Button";
 function Hero() {
   return (
     <div className="hero-container">
-      <video src="videos/video-1.mp4" autoPlay loop muted></video>
       <h1>ADVENTURE AWAITS</h1>
       <p>What are you waiting for?</p>
       <div className="hero-btns">
